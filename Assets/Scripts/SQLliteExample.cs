52298f3a62d6547bde1523ab262cf575b87c3233
Fabio-Mnte <fabio.mnte@gmail.com>
2024-05-03 20:26:54 -0300

    update
    salvando  as alterações feitas hoje
    adicionado path banco de dados

--- Assets/Scripts/SQLliteExample.cs
+++ Assets/Scripts/SQLliteExample.cs
@@ -14,12 +14,7 @@ public class SQLiteManager : MonoBehaviour
     void Start()
     {
         OpenConnection();
-        CreateTable();
-        //InsertData("Arthur");
-        //InsertData("Leonardo");
-        //PrintData();
-        //DeleteData(1);
-        
+        CreateTable();        
     }
 
     private void OpenConnection()
@@ -32,7 +27,7 @@ public class SQLiteManager : MonoBehaviour
     {
        
         var command = connection.CreateCommand();
-        command.CommandText = "CREATE TABLE IF NOT EXISTS history (history_id INTEGER  PRIMARY KEY, nome VARCHAR); CREATE TABLE IF NOT EXISTS fase (fase_id INTEGER  PRIMARY KEY ,history_id INT,nome VARCHAR,FOREIGN KEY (history_id) REFERENCES history(history_id)); CREATE TABLE IF NOT EXISTS questao (questao_id INTEGER  PRIMARY KEY , fase_id INT, FOREIGN KEY (fase_id) REFERENCES fase(fase_id)); CREATE TABLE IF NOT EXISTS opcoes (opcoes_id INTEGER  PRIMARY KEY , questao_id INT, FOREIGN KEY (questao_id) REFERENCES questao(questao_id));";
+        command.CommandText = "CREATE TABLE IF NOT EXISTS history (history_id INTEGER  PRIMARY KEY, nome VARCHAR); CREATE TABLE IF NOT EXISTS fase (fase_id INTEGER  PRIMARY KEY ,history_id INT,nome VARCHAR,FOREIGN KEY (history_id) REFERENCES history(history_id)); CREATE TABLE IF NOT EXISTS questao (questao_id INTEGER  PRIMARY KEY , fase_id INT, questao_enunciado TEXT, FOREIGN KEY (fase_id) REFERENCES fase(fase_id)); CREATE TABLE IF NOT EXISTS opcoes (opcoes_id INTEGER  PRIMARY KEY, questao_id INT,  opcao_texto TEXT, correta INTEGER, FOREIGN KEY (questao_id) REFERENCES questao(questao_id));";
     
         command.ExecuteNonQuery();
         
@@ -49,9 +44,6 @@ public class SQLiteManager : MonoBehaviour
         var command = connection.CreateCommand();
         command.CommandText = $"INSERT INTO history (nome) VALUES ('{name}')";
         command.ExecuteNonQuery();
-    }
-    public void drop_tables(){
-
     }
     public void insert_test(){
         var command = connection.CreateCommand();
