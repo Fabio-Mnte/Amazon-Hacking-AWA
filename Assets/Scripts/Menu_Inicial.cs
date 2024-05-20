7ea115039269e7a23fb907841d1e461656aef178
Enzo Martins <martinsenzo.net@icloud.com>
2024-05-20 01:02:24 -0300

    Colagem Tela Inicial e Botao de Sair

---
+++ Assets/Scripts/Menu_Inicial.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Inicial : MonoBehaviour
+{
+    void QuitGame() {
+        Application.Quit();
+    }
+}
